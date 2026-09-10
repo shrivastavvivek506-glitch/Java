82a785eba98839b8efaa9016e67af09154ab2a36
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-09-10 23:59:34 +0530

    add

---
+++ starting java/Add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Add {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(10);
+        int b = sc.nextInt(20);
+        System.out.println("Sum = " + (a + b));
+
+    }
+
+}
