ed540d73c5714e66981e26f7d04b945b12e2d72e
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-09-13 23:40:43 +0530

    Add

---
+++ starting java/Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    public static void main(String[] args) {
+         
+        int a = 12;
+        int b = 45;
+
+        int sum = a + b;
+        
+        System.out.println("sum = " + sum);
+    }
+    
+}
