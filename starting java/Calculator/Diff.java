85939c9770ddad3c03656572d7a59b2912520dc4
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-09-15 23:50:18 +0530

    Diffrence

---
+++ starting java/Calculator/Diff.java
@@ -0,0 +1,12 @@
+package Calculator;
+
+public class Diff {
+    public static void main(String[] args) {
+        
+        int a = 34;
+        int b = 5;
+        int diff = a - b;
+
+        System.out.println("Difference = " + diff);
+    }    
+}
