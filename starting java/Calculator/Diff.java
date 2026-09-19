ff722a0f5fc3d2592f791cb19bbc9c42008152a6
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-09-19 23:47:42 +0530

    add

--- starting java/Calculator/Diff.java
+++ starting java/Calculator/Diff.java
@@ -1,5 +1,3 @@
-package Calculator;
-
 public class Diff {
     public static void main(String[] args) {
         
---
+++ starting java/Calculator/User.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class User {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        System.out.println("Enter the first number: " + x);
+
+        int y = sc.nextInt();
+        System.out.println("Enter the second number: " + y);
+    }
+}
