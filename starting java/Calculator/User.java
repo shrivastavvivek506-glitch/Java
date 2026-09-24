bc42b3f37f2e763bfc2324f5552c9b088d7d8680
Vivek shrivastav <shrivastavvivek506@gmail.com>
2026-09-24 19:33:39 +0530

    Sum of number  input in user

--- starting java/Calculator/User.java
+++ starting java/Calculator/User.java
@@ -4,11 +4,12 @@ public class User {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        int x = sc.nextInt();
-        System.out.println("Enter the first number: " + x);
+        int Sum = a + b;
 
-        int y = sc.nextInt();
-        System.out.println("Enter the second number: " + y);
+        System.out.println(Sum);
+
     }
 }
